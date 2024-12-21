54c996d3ab1bd6500a2885c4031ecbca54c3ac52
Ivan Ivanov <ivantimesthree@gmail.com>
2024-12-21 19:30:54 +0000

    Added solution for singly linked list middle element (Tortoise and Hare algorithm)

---
+++ python/1_algorithms/05_linked_lists/2_singly_linked_list_middle.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def find_middle(self):
+        if self.head is None:
+            return None
+
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+
+def main():
+    linked_list = LinkedList()
+    (linked_list.append(x) for x in [1, 2, 3, 4, 5, 6])
+    sth = linked_list.find_middle()
+    print(f"sth: {sth}")
+
+
+if __name__ == "__main__":
+    main()
